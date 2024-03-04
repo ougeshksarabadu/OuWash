ee2429404cfcff409e1b41061a1704873811d15f
Eugenier Elisa <theeugenieelisa@gmail.com>
2024-03-04 16:34:46 +0400

    car_washes_controller

--- app/controllers/car_washes_controller.rb
+++ app/controllers/car_washes_controller.rb
@@ -1,4 +1,26 @@
 class CarWashesController < ApplicationController
+  def new
+    @car_wash = Car_wash.new
+  end
 
+  def create
+    @car_wash = Car_wash.new(car_wash_params)
+    @car_wash.save
+    redirect_to car_wash_path(@car_washes)
+  end
+
+  def index
+    @car_washes = Car_wash.all
+  end
+
+  def show
+    @car_wash = Car_wash.find(params {:id})
+  end
+
+  private
+
+  def car_wash_params
+    params.required(:car_wash).permit(:name, :price_sedan, :price_suv, :price_truck, :price_luxury)
+  end
 
 end
