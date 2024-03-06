0e812a07d71832be1015ba43eef1c7173c1cf195
ougesh k.sarabadu <ougeshksarabadu@gmail.com>
2024-03-06 15:48:03 +0400

    appointment fixed

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,7 @@
 class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
 
   def show
     @appointment = Appointment.find(params[:id])
@@ -6,13 +9,14 @@ class AppointmentsController < ApplicationController
 
   def new
     @appointment = Appointment.new
+    @car_wash = CarWash.find(params[:car_wash_id])
   end
 
   def create
     @appointment = Appointment.new(appointment_params)
 
     if @appointment.save
-      redirect_to @appointment
+      redirect_to @appointment, notice: "Appointment was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
